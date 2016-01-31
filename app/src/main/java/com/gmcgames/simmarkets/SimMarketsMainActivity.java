ed3a7c875f1f24bc1c03ce6a00050508f29f514b
Carlos Fadón <carlosfadon@MacBook-Pro-de-Carlos-Fadon.local>
2016-01-31 20:00:33 +0100

    Initial commit

---
+++ app/src/main/java/com/gmcgames/simmarkets/SimMarketsMainActivity.java
@@ -0,0 +1,13 @@
+package com.gmcgames.simmarkets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimMarketsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sim_markets_main);
+    }
+}
